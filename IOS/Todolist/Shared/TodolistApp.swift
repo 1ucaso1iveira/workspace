fc29b19cda5ffe07bc15e09d9dd806e3b2ae7fbf
lucaoliver <lucasoliveiraahh@gmail.com>
2022-04-03 17:33:51 -0300

    first IOS App

---
+++ IOS/Todolist/Shared/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Shared
+//
+//  Created by Lucas Oliveira on 02/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
